60d3ecef7e5c7d443abd84ab2878d45a5e2473a0
Dan4kpromax28 <danikbalik@gmail.com>
2024-03-14 12:01:43 +0200

    Update: Page update

---
+++ src/model/Page.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Page {
+    private String title;
+    private String description;
+    private ArrayList<User> followers= new ArrayList<User>();
+    private ArrayList<Post> postsInPage = new ArrayList<Post>();
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if (title != null && title.length() > 5 && 50 > title.length()){
+            this.title = title;
+        }else
+            this.title = "New title";
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        if(description!=null && description.length() > 5 && description.length() < 500) {
+            this.description = description;
+        }else
+            this.description = "_________";
+
+    }
+
+    public ArrayList<User> getFollowers() {
+        return followers;
+    }
+
+
+
+    public ArrayList<Post> getPostsInPage() {
+        return postsInPage;
+    }
+
+    public Page(){
+        setTitle("Autoserviss Ventspili");
+        setDescription("Autoserviss Ventspioli - jaunakas aktualitates");
+    }
+
+    public String toString(){
+        return title+"(" + description + ") -> "+ followers.size();
+    }
+}
